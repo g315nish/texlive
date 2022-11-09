223508bc7a89ebf1c83ce67126f25f5c9bf657e1
g315nish <g520315m@mails.cc.ehime-u.ac.jp>
2022-11-09 16:52:37 +0900

    first commit

---
+++ 2019/texmf-var/luatex-cache/context/a86c089b384a3076dc514ba966a1fac9/trees/0269492976cc3e58a475a712c38a267b.lua
@@ -0,0 +1,18 @@
+return {
+ ["content"]={
+  ["files"]={},
+  ["metadata"]={
+   ["directories"]=27,
+   ["files"]=0,
+   ["path"]="E:/texlive/texmf-local",
+   ["remappings"]=0,
+  },
+  ["remap"]={},
+ },
+ ["date"]="2019-10-11",
+ ["root"]="E:/texlive/texmf-local",
+ ["time"]="15:31:21",
+ ["type"]="files",
+ ["uuid"]="c39c1f40-4a31-88bb-e8c8-99781e12812e",
+ ["version"]="1.100",
+}
---
+++ 2019/texmf-var/luatex-cache/context/a86c089b384a3076dc514ba966a1fac9/trees/9d8576a18c59fc65c42b8c6d0dc5211a.lua
@@ -0,0 +1,22 @@
+return {
+ ["content"]={
+  ["files"]={
+   ["ls-r"]="",
+  },
+  ["metadata"]={
+   ["directories"]=0,
+   ["files"]=1,
+   ["path"]="selfautoparent:texmf-config",
+   ["remappings"]=1,
+  },
+  ["remap"]={
+   ["ls-r"]="ls-R",
+  },
+ },
+ ["date"]="2019-10-11",
+ ["root"]="selfautoparent:texmf-config",
+ ["time"]="15:31:21",
+ ["type"]="files",
+ ["uuid"]="562ad9e0-4a51-b8b8-623c-07827692fa98",
+ ["version"]="1.100",
+}
